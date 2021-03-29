9c46bf306d81a271a4b1baba0ad50fcf00c87aca
Eliza  Sillesky <els95861@csci-odin.cs.uga.edu>
2021-03-29 12:26:42 -0400

    added methods in

---
+++ BinaryTree.cpp
@@ -0,0 +1,53 @@
+#include "BinaryTree.h"
+#include <string>
+
+using namespace std;
+
+template <class T>
+BinaryTree<T>::BinaryTree() {
+
+}
+
+template <class T>
+BinaryTree<T>::~BinaryTree() {
+
+}
+
+template <class T>
+void BinaryTree<T>::insert(T &key) {
+
+}
+
+template <class T>
+void BinaryTree<T>::deleteItem(T &key) {
+
+}
+
+template <class T>
+void BinaryTree<T>::retrieve(T &item, bool &found) const {
+
+}
+
+template <class T>
+void BinaryTree<T>::preOrder() const {
+
+}
+
+template <class T>
+void BinaryTree<T>::inOrder() const {
+
+}
+
+template <class T>
+void BinaryTree<T>::postOrder() const {
+
+}
+
+template <class T>
+int BinaryTree<T>::getLength() const {
+
+}
+
+template class BinaryTree<int>;
+template class BinaryTree<float>;
+template class BinaryTree<std::string>;
---
+++ BinaryTree.h
@@ -0,0 +1,29 @@
+using namespace std;
+
+template <class T>
+struct NodeType {
+  T key;
+  NodeType<T> *left;
+  NodeType<T> *right;
+};
+
+template <class T>
+class BinaryTree {
+public:
+  BinaryTree();
+  ~BinaryTree();
+  void insert(T &key);
+  void deleteItem(T &key);
+  void retrieve(T &item, bool &found) const;
+  void preOrder() const;
+  void inOrder() const;
+  void postOrder() const;
+  int getLength() const;
+
+  //getNumSingleParent();
+  //getNumLeafNodes();
+  //getSumOfSubtrees();
+  
+ private:
+  NodeType<T> *root; //not sure if right
+};
